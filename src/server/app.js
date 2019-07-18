6430fcb650221ab310e283671e77fc1cc937761d
fatbros <02luvsic@gmail.com>
2019-07-18 23:18:37 +0900

    add editorconfig

--- src/server/app.js
+++ src/server/app.js
@@ -12,26 +12,26 @@ const config = require('../../webpack.config');
 app.use(express.static('../../build'));
 
 if (process.env.NODE_ENV) {
-    config.entry.app.unshift('webpack-hot-middleware/client?reload=true&timeout=1000');
-    config.plugins.push(new webpack.HotModuleReplacementPlugin());
-    const compiler = webpack(config);
-    app.use(webpackDevMiddleware(compiler, {
-        publicPath: config.output.publicPath
-    }));
-    app.use(webpackHotMiddleware(compiler));
+  config.entry.app.unshift('webpack-hot-middleware/client?reload=true&timeout=1000');
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  const compiler = webpack(config);
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+  }));
+  app.use(webpackHotMiddleware(compiler));
 }
 
-app.get('/' , function(req, res){
-    res.sendFile(__dirname+'/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection',function(socket){
-    socket.on('message',function(msg){
-        console.log('message: ' + msg);
-        io.emit('message', msg);
-    });
+io.on('connection', function (socket) {
+  socket.on('message', function (msg) {
+    console.log('message: ' + msg);
+    io.emit('message', msg);
+  });
 });
 
-http.listen(PORT, function(){
-    console.log('server listening. Port:' + PORT);
+http.listen(PORT, function () {
+  console.log('server listening. Port:' + PORT);
 });
