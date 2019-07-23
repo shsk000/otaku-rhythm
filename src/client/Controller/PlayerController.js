04929e25027b15f7ef2621f5016e92dc81608ef6
fatbros <02luvsic@gmail.com>
2019-07-23 22:55:05 +0900

    add player method template

---
+++ src/client/Controller/PlayerController.js
@@ -0,0 +1,15 @@
+class PlayerController {
+  constructor() {
+    this.isPlayableVideo = false;
+    // this.player = new Player()
+  }
+
+  onSocketEvent() {}
+  onServerPlayVideo() {}
+  onServerLoadVideo() {}
+  onServerStopVideo() {}
+  emitPlayVideo() {}
+  emitPlayVideoStatus() {}
+}
+
+module.exports = PlayerController;
